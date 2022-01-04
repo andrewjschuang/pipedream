b6d1fac08491b63f6af4c70b4c6d9c17b24615ec
Andrew Chuang <andrewjschuang@gmail.com>
2022-01-04 17:37:13 -0300

    github and export demo

--- components/test/action.js
+++ components/test/action.js
@@ -1,14 +1,29 @@
-import axios from "axios";
+import { Octokit } from '@octokit/rest';
 
 export default {
   name: "Action Demo",
   description: "This is a demo action",
   key: "action_demo",
-  version: "0.0.3",
+  version: "0.0.4",
   type: "action",
-  props: {},
-  async run() {
-    const response = await axios.get(`https://swapi.dev/api/people/1/`);
-    return `hello ${response.data.name}`
+  props: {
+    github: {
+      type: "app",
+      app: "github"
+    }
+  },
+  async run({ $ }) {
+    const octokit = new Octokit({
+      auth: this.github.$auth.oauth_access_token
+    });
+
+    const { data } = await octokit.repos.get( {
+      owner: `pipedreamhq`,
+      repo: `pipedream`
+    });
+
+    $.export("$summary", `Successfully fetched info for \`${data.full_name}\``);
+
+    return data;
   },
 }
